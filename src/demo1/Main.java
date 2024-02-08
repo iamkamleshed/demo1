6497a347509f1411400ef50e7086283b669677fb
Kkamleshkandel123 <57823892+Kkamleshkandel123@users.noreply.github.com>
2024-02-08 13:42:06 -0500

    This is my fourth commit

--- src/demo1/Main.java
+++ src/demo1/Main.java
@@ -7,7 +7,14 @@ public class Main {
 		
 		System.out.println("hello1");
 		System.out.println("hello2");
+		int a =5;
+		int b = 6;
 		
+		if(a>b) {
+			System.out.println("a is greater");
+		}else {
+			System.out.println("b is greater");
+		}
 
 	}
 
